72593d0594d2125a220f1170b3af246396bb54ac
ngtranhoan <156505983+ngtranhoan@users.noreply.github.com>
2024-03-01 22:02:00 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,50 +1,83 @@
-const fs = require("fs-extra");
-const login = require("helyt");
-const readline = require("readline");
-const totp = require("totp-generator");
-
-let configPath = "";
-let argv = process.argv.slice(2);
-if (argv.length !== 0) configPath = argv[0];
-else configPath = "./config.json";
-
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-const option = {
-	logLevel: "silent",
-	forceLogin: true,
-	userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-};
-
-const config = require(`./${configPath}`);
-let email = config.EMAIL;
-let password = config.PASSWORD;
-let otpkey = config.OTPKEY.replace(/\s+/g, '').toLowerCase();
-
-login({ email, password }, option, (err, api) => {
-	if (err) {
-		switch (err.error) {
-			case "login-approval":
-				if (otpkey) err.continue(totp(otpkey));
-				else {
-					console.log("Nhập mã xác minh 2 lớp:");
-					rl.on("line", line => {
-						err.continue(line);
-						rl.close();
-					});
-				}
-				break;
-			default:
-			console.error(err);
-			process.exit(1);
-		}
-		return;
-	}
-	const json = JSON.stringify(api.getAppState());
-	fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
-	console.log("Đã ghi xong appstate!");
-	process.exit(0);
-});
+[
+    {
+        "key": "ps_n",
+        "value": "0",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.899Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "datr",
+        "value": "Zl2SZQQvKHsuvQIfq7tfHd4W",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "sb",
+        "value": "Zl2SZd5kOyf2M0UPWGYCZpoh",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B756x412x3.178847074508667",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "wd",
+        "value": "809x1484",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61556275671238",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "dpr",
+        "value": "3.178847074508667",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "xs",
+        "value": "25%3AIjtk8GeukJeF7w%3A2%3A1708616735%3A-1%3A11300%3A%3AAcXrBvx6FaZ4eiyvfKYk09hMVfRp5wIiSfy_GUT6Ng",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    },
+    {
+        "key": "fr",
+        "value": "1OZ394ebnoBoDkDUf.AWUJ8lL_6s8-GJ8wbevTnTBm2eA.Bl4ddh..AAA.0.0.Bl4ddh.AWU5OdmAw2M",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-03-01T15:01:13.900Z",
+        "lastAccessed": "2024-03-01T15:01:13.900Z"
+    }
+]
